a83dd5069fe31138b92186fe955b26ab21f11947
Antony Jordan Villar Cristobal <antony.villar.rp@viaexperis.pe>
2023-10-04 13:05:34 -0500

    first commit.

---
+++ constants.js
@@ -0,0 +1,39 @@
+export const BLOCK_SIZE = 20;
+export const BOARD_WIDTH = 14;
+export const BOARD_HEIGHT = 30;
+
+export const piece = {
+  position: { x: 5, y: 5},
+  shape: [
+    [1, 1],
+    [1, 1],
+  ]
+};
+
+export const PIECES = [
+  [
+    [1, 1],
+    [1, 1]
+  ],
+  [
+    [1, 1, 1, 1],
+  ],
+  [
+    [0, 1, 0],
+    [1, 1, 1,]
+  ],
+  [
+    [1, 1, 0],
+    [0, 1, 1]
+  ],
+  [
+    [1, 0],
+    [1, 0],
+    [1, 1]
+  ],
+  [
+    [0, 1],
+    [0, 1],
+    [0, 1],
+  ]
+];
